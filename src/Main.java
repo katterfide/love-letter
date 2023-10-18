1f4e49d970b736e16e89737b6b4df49942b6910e
faltenburg <altenburg@cip.ifi.lmu.de>
2023-10-19 01:46:25 +0200

    Game countdown, playCommands

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws InterruptedException {        //brauche throws interrupted Exception weil sonst sleep nicht akzeptiert wird
         Scanner sc = new Scanner(System.in);
         String choice;
         int playerCount;
@@ -43,11 +42,58 @@ public class Main {
                 }
 
 
-                String[] playerNames = new String[playerCount + 1];
+                String[] playerNames = new String[playerCount + 1];     //ACHTUNG ARRAY SIZE  HIER playerCount +1! weil ich mit i = 1 anfange und array sonst out of bounds geht
                 for (int i = 1; i <= playerCount; i++) {
                     System.out.print("Enter player name #" + i + ": ");
                     playerNames[i] = sc.next();
                 }
+
+                System.out.println("We're all set!");
+                Thread.sleep(1000);
+                System.out.println("Get ready to start playing in 3...");
+                Thread.sleep(1000);
+                System.out.println("Get ready to start playing in 2...");
+                Thread.sleep(1000);
+                System.out.println("Get ready to start playing in 1...");
+                Thread.sleep(1000);
+                System.out.println("Let's go!");
+
+                System.out.println("For a list of available commands type: " + "\\" + "help");
+
+        label:
+        while(true) {
+            choice = sc.next();
+
+            switch (choice) {
+                case "\\" + "help":
+                    System.out.println("\\" + "help to display a list of available commands.");
+                    System.out.println("\\" + "playCard to play a card from your Hand");
+                    System.out.println("\\" + "showHand to show your current cards");
+                    System.out.println("\\" + "showScore to show the scoreboard");
+                    break;
+
+                case "\\" + "showHand":
+                    System.out.println("you have a <card1> and a <card 2> on hand.");
+                    break;
+
+                case "\\" + "showScore":
+                    //Score array
+                    System.out.println("<scoreArray> with playerNames");
+                    break;
+
+                case "\\" + "playCard":
+                    //which card?
+                    //against who?
+                    System.out.println("you have played <playedCard> against <playerName i>");
+                    System.out.println("your turn is over.");
+                    break label;
+
+                default:
+                    System.out.println("Command not recognized.");
+                    System.out.println("For a list of available commands type: " + "\"" + "\\" + "help" + "\"");
+                    break;
+            }
+        }
 
 
     }
