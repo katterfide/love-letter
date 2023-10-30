7bf1cbad42e59eb2b9a02f29df7a14daebebe158
faltenburg <altenburg@cip.ifi.lmu.de>
2023-10-30 16:55:18 +0100

    clean LL3 backup to clear main

--- src/Player.java
+++ src/Player.java
@@ -13,24 +13,6 @@ public class Player {
     static int currentPlayerIndex = 0; // Define the index of the current player
     static String currentPlayer = Main.playerNames[currentPlayerIndex];
 
-    public static int inputPlayerNumber(Scanner sc) {
-        int playerCount;
-
-        while (true) {
-            System.out.println("How many players are there? (2-4)");
-            playerCount = sc.nextInt();
-
-            if (playerCount > 4) {
-                System.out.println("That is too many players. Please limit yourself to playing with 2-4 players.");
-            } else if (playerCount < 2) {
-                System.out.println("That is too little players. Please limit yourself to playing with 2-4 players.");
-            } else {
-                break;
-            }
-        }
-
-        return playerCount;
-    }
 
 
 
